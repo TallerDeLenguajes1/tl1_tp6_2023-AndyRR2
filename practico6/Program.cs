b169461bcb01a7a58da851e740306f8b98355d9c
Andy Rodriguez <andyarr222@gmail.com>
2023-05-20 09:59:03 -0300

    arreglos

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,15 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
-/*int a;
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+int a;
 int b;
 a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);*/
-
-
-
-
-
-
-
-
+Console.WriteLine("valor de b:"+b);
