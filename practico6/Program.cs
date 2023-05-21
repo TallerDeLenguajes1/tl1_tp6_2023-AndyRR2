1ff74aa001b0ef79c360f9fd2b4d625f185d89e0
Andy Rodriguez <andyarr222@gmail.com>
2023-05-20 23:31:33 -0300

    program.cs

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -58,3 +58,83 @@ while (opcion!=0){
     float.TryParse(Console.ReadLine(),out opcion);
 }
 Console.WriteLine("Cerrado");*/
+
+//#3
+string? entrada1,entrada2;
+float a, b, opcion;
+bool resultado;
+Console.WriteLine("CalculadoraV2: ");
+Console.WriteLine("Entre un numero");
+entrada1 = Console.ReadLine();
+resultado = float.TryParse(entrada1,out a);
+while (resultado==false){
+    Console.WriteLine("Numero invalido");
+    Console.WriteLine("Entre un numero: ");
+    entrada1 = Console.ReadLine();
+    resultado = float.TryParse(entrada1,out a);
+}
+Console.WriteLine("Menu:");
+Console.WriteLine("1-Valor absoluto");
+Console.WriteLine("2-Cuadrado");
+Console.WriteLine("3-Raiz cuadrada");
+Console.WriteLine("4-Seno");
+Console.WriteLine("5-Coseno");
+Console.WriteLine("6-Parte entrea");
+Console.WriteLine("Otro-Cerrar");
+float.TryParse(Console.ReadLine(),out opcion);
+while (opcion!=0){
+    switch (opcion){  
+        case 1: if(a<0){
+                Console.WriteLine(-1*a);
+                }else{
+                    Console.WriteLine(a);
+                };
+        break;
+        case 2: Console.WriteLine(a*a);break;
+        case 3: Console.WriteLine(Math.Sqrt(a));break;
+        case 4: Console.WriteLine(Math.Sin(a));break;
+        case 5: Console.WriteLine(Math.Cos(a));break;
+        case 6: Console.WriteLine(Convert.ToInt32(a));break;
+    }    
+    Console.WriteLine("Ver maximo y minimo: ");
+    Console.WriteLine("Entre un numero");
+    entrada1 = Console.ReadLine();
+    resultado = float.TryParse(entrada1,out a);
+    while (resultado==false){
+    Console.WriteLine("Numero invalido");
+    Console.WriteLine("Entre un numero: ");
+    entrada1 = Console.ReadLine();
+    resultado = float.TryParse(entrada1,out a);
+    }
+    Console.WriteLine("Entre el segundo numero");
+    entrada2 = Console.ReadLine();
+    resultado = float.TryParse(entrada2,out b);
+    while (resultado==false){
+        Console.WriteLine("Numero invalido");
+        Console.WriteLine("Entre el segundo numero");
+        entrada2 = Console.ReadLine();
+        resultado = float.TryParse(entrada2,out b);
+    }
+    Console.WriteLine("Maximo: " + Math.Max(a,b));
+    Console.WriteLine("Minimo: " + Math.Min(a,b));
+    Console.WriteLine("CalculadoraV2: ");
+    Console.WriteLine("Entre un numero");
+    entrada1 = Console.ReadLine();
+    resultado = float.TryParse(entrada1,out a);
+    while (resultado==false){
+        Console.WriteLine("Numero invalido");
+        Console.WriteLine("Entre un numero: ");
+        entrada1 = Console.ReadLine();
+        resultado = float.TryParse(entrada1,out a);
+    }
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1-Valor absoluto");
+    Console.WriteLine("2-Cuadrado");
+    Console.WriteLine("3-Rauz cuadrada");
+    Console.WriteLine("4-Seno");
+    Console.WriteLine("5-Coseno");
+    Console.WriteLine("6-Parte entrea");
+    Console.WriteLine("Otro-Cerrar");
+    float.TryParse(Console.ReadLine(),out opcion);
+}
+Console.WriteLine("Cerrado");
